94aa4dabf995f6b4f883745f3695eb311dd56e27
liminghao <liminghao@xiaomi.com>
2014-07-21 14:39:08 +0800

    build: add the prebuilt core gnu c libraries to yudatun.
    N/A
    
    Change-Id: I3fcb0e656a1dc105585021371f86fb69d7925a5b
    Signed-off-by: liminghao <liminghao@xiaomi.com>

--- target/product/embedded.mk
+++ target/product/embedded.mk
@@ -29,3 +29,15 @@ CORE_LUA_FILES += \
     actions.lua
 
 PRODUCT_PACKAGES += $(CORE_LUA_FILES)
+
+
+ifeq (arm-none-linux-gnueabi, $(strip $(TARGET_LINUX_EABI_PREFIX)))
+
+PRODUCT_PACKAGES += \
+    ld-linux.so.3 \
+    libm.so.6 \
+    libc.so.6 \
+    libgcc_s.so.1 \
+    libstdc++.so.6
+
+endif # TARGET_LINUX_EABI_PREFIX
