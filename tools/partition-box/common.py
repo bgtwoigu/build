ca5bb7a810931410d5711d0f4697fe365878eace
liminghao <liminghao@xiaomi.com>
2015-09-02 14:26:04 +0800

    partition-box: replace 4 spaces to 2 spaces into python scripts.
    N/A
    
    Change-Id: Ibd4d28431220ab9dd046d343823fb3d0d2fda828
    Signed-off-by: liminghao <liminghao@xiaomi.com>

--- tools/partition-box/common.py
+++ tools/partition-box/common.py
@@ -18,43 +18,42 @@ COMMON_DOCSTRING = """
 """
 
 def usage(docstring):
-    print docstring.rstrip("\n")
-    print COMMON_DOCSTRING
+  print docstring.rstrip("\n")
+  print COMMON_DOCSTRING
 
 def parseOptions(argv, docstring,
                  extra_opts="", extra_long_opts=(),
                  extra_option_handler=None):
-    """Parse the options in argv and return any arguments that aren't
-    flags. docstring is the calling module's docstring, to be displayed
-    for errors and -h. extra_opts and extra_long_opts are for flags
-    defined by the caller, which are processed by passing them to
-    extra_option_handler"""
+  """Parse the options in argv and return any arguments that aren't
+  flags. docstring is the calling module's docstring, to be displayed
+  for errors and -h. extra_opts and extra_long_opts are for flags
+  defined by the caller, which are processed by passing them to
+  extra_option_handler"""
 
-    try:
-        opts, args = getopt.getopt(
-            argv, "hv" + extra_opts,
-            ["help", "verbose",] +
-            list(extra_long_opts))
-    except getopt.GetoptError, err:
-        usage(docstring)
-        print "**", str(err), "**"
-        sys.exit(2)
+  try:
+    opts, args = getopt.getopt(argv, "hv" + extra_opts,
+                               ["help", "verbose",] +
+                               list(extra_long_opts))
+  except getopt.GetoptError, err:
+    usage(docstring)
+    print "**", str(err), "**"
+    sys.exit(2)
 
-    for opt, arg in opts:
-        if opt in ("-h", "--help"):
-            usage(docstring)
-            sys.exit()
-        elif opt in ("-v", "--verbose"):
-            OPTIONS.verbose = True
-        else:
-            if extra_option_handler is None or not extra_option_handler(opt, arg):
-                assert False, "unknown option \"%s\"" % (opt,)
+  for opt, arg in opts:
+    if opt in ("-h", "--help"):
+      usage(docstring)
+      sys.exit()
+    elif opt in ("-v", "--verbose"):
+      OPTIONS.verbose = True
+    else:
+      if extra_option_handler is None or not extra_option_handler(opt, arg):
+        assert False, "unknown option \"%s\"" % (opt,)
 
-    return args
+  return args
 
 def run(args, **kwargs):
-    """Create and return a subprocess.Popen object, printing the command
-    line on the terminal if -v was specified."""
-    if OPTIONS.verbose:
-        print "  running: ", " ".join(args)
+  """Create and return a subprocess.Popen object, printing the command
+  line on the terminal if -v was specified."""
+  if OPTIONS.verbose:
+    print "  running: ", " ".join(args)
     return subprocess.Popen(args, **kwargs)
