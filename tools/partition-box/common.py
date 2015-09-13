9182431cbebf7b7f0f13e568584137e2d2c27e2a
liminghao <yudatun@gmail.com>
2015-09-13 22:55:13 +0800

    build: add license for common.py.
    N/A
    
    Change-Id: Id922da8dff70831466e30386630021f43ad4f1c4
    Signed-off-by: liminghao <yudatun@gmail.com>

--- tools/partition-box/common.py
+++ tools/partition-box/common.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2015 The Yudatun Open Source Project
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation
+#
+
 import os
 import getopt
 import subprocess
